0ea0e3726d8fcc9dd36cd4b43fbb29a82c9240f2
Martin Emmert <martin@martinemmert.de>
2017-04-11 13:30:04 +0200

    refactored the nodejs implementation to call exec async, callback argument is now required

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const dockerLambda = require("docker-lambda");
 const pkg = require("./package.json");
 const shell = require("shelljs");
 
-module.exports = function (outputStats) {
-  let res = shell.exec("./bin/invoke-docker-lambda", {
-    silent: true
-  });
-  
-  if (res.code == 1) {
-    // remove any stderr icons
-    let err = res.stderr.split("\n").slice(0, -1).map(e => e.replace(/[ℹ✔⚠✖]\s/, ""));
-    if (outputStats) console.info(res.stderr);
-    return {"error": err, "result": res.stdout};
+module.exports = function (outputStats, callback) {
+  if (!callback) {
+    throw new Error("callback function is missing");
   }
+  shell.exec(__dirname + "/bin/invoke-docker-lambda", {silent: true}, (code, stdout, stderr) => {
+    if (code == 1) {
+      // remove any stderr icons
+      let err = stderr.split("\n").slice(0, -1).map(e => e.replace(/[ℹ✔⚠✖]\s/, ""));
+      if (outputStats) console.info(stderr);
+      callback({
+        "error": err,
+        "result": stdout
+      });
+      return;
+    }
 
-  if (outputStats) console.info(res.stderr);
-  return {result: res.stdout};
+    if (outputStats) console.info(stderr);
+    callback({
+      result: stdout
+    });
+  });
 }
