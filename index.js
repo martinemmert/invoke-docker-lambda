089425be40a75736151a966f87580fa6bd7e4aef
Martin Emmert <martin@martinemmert.de>
2017-04-11 10:41:17 +0200

    added wrapper function to call the bin from within node

--- index.js
+++ index.js
@@ -2,7 +2,21 @@
 
 const path = require("path");
 const dockerLambda = require("docker-lambda");
-const pkg = require("package.json");
+const pkg = require("./package.json");
+const shell = require("shelljs");
 
-console.log(pkg);
+module.exports = function (outputStats) {
+  let res = shell.exec("./bin/invoke-docker-lambda", {
+    silent: true
+  });
+  
+  if (res.code == 1) {
+    // remove any stderr icons
+    let err = res.stderr.split("\n").slice(0, -1).map(e => e.replace(/[ℹ✔⚠✖]\s/, ""));
+    if (outputStats) console.info(res.stderr);
+    return {"error": err, "result": res.stdout};
+  }
 
+  if (outputStats) console.info(res.stderr);
+  return {result: res.stdout};
+}
